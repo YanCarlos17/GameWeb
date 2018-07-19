74e3f6b359ed9719569d679052aa4982a5308f2f
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-07-19 18:05:07 -0500

    --

--- js/init.js
+++ js/init.js
@@ -6,7 +6,7 @@ function init(){
    var canvas = document.getElementById('micanvas');
    var ctx = canvas.getContext('2d');
    var img = document.getElementById('shell');
-   ctx.drawImage(img, -140, -10);
+   ctx.drawImage(img, 125, 120, 50, 30);
 }
 function a_radianes(grados){
     return(grados * Math.PI) / 180;
