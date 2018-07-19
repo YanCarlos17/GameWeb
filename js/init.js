cd5c26009b6e93f7d399376265d8481f858cb9c5
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-07-19 16:44:04 -0500

    Aprendizaje del uso del canvas y atributo del contexto(JavaScript)

--- js/init.js
+++ js/init.js
@@ -1,4 +1,16 @@
 //DOM - DocumentObjectModel
-document.addEventListener('DOMContentLoaded', function(){
-    
-}, false);
+window.addEventListener('load', init);
+function init(){
+   var canvas = document.getElementById('micanvas');
+   var ctx = canvas.getContext('2d');
+   ctx.fillStyle='#01DFD7';
+   ctx.fillRect(120,110,20,15);
+   ctx.fillStyle='#01DFD7';
+   ctx.fillRect(100,100,20,15);
+   ctx.strokeStyle='yellowgreen';
+   ctx.lineWidth = 1;  
+   ctx.strokeRect(150,110,20,15);
+   ctx.fillStyle='#01DFD7';
+   ctx.fillRect(80,110,20,15);
+   console.log(canvas);
+}
