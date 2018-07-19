88bca2464cbc843d3fdf765d598f6f5efad0f444
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-07-19 17:59:19 -0500

    Se agrega una imagen dentro del canvas con javaScript

--- js/init.js
+++ js/init.js
@@ -1,16 +1,14 @@
 //DOM - DocumentObjectModel
 window.addEventListener('load', init);
+
 function init(){
+   var grados; 
    var canvas = document.getElementById('micanvas');
    var ctx = canvas.getContext('2d');
-   ctx.fillStyle='#01DFD7';
-   ctx.fillRect(120,110,20,15);
-   ctx.fillStyle='#01DFD7';
-   ctx.fillRect(100,100,20,15);
-   ctx.strokeStyle='yellowgreen';
-   ctx.lineWidth = 1;  
-   ctx.strokeRect(150,110,20,15);
-   ctx.fillStyle='#01DFD7';
-   ctx.fillRect(80,110,20,15);
-   console.log(canvas);
+   var img = document.getElementById('shell');
+   ctx.drawImage(img, -140, -10);
 }
+function a_radianes(grados){
+    return(grados * Math.PI) / 180;
+}
+
