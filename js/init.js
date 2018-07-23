ceb68d40b63116582aefa11af50ca1c2d46a4891
yancarlos Solarte <ycsolarte8@misena.edu.co>
2018-07-23 13:47:19 -0500

    Ajuste para canvas y intentar animacion

--- js/init.js
+++ js/init.js
@@ -1,14 +1,33 @@
 //DOM - DocumentObjectModel
-window.addEventListener('load', init);
+var velocidad=5;
+var direccion= velocidad;
+var inciar=false;
+var x = 50;
+var y = 10;
+var intervalo;
 
+window.addEventListener('load', init);
 function init(){
-   var grados; 
-   var canvas = document.getElementById('micanvas');
-   var ctx = canvas.getContext('2d');
-   var img = document.getElementById('shell');
-   ctx.drawImage(img, 125, 120, 50, 30);
+var canvas = document.getElementById('micanvas');
+
+var ctx = canvas.getContext('2d'); 
+document.getElementById('boton').addEventListener('click', function(){
+    if(iniciar){
+        this.value='iniciar';
+        window.clearInterval(intervalo);
+    }
+});  
 }
-function a_radianes(grados){
-    return(grados * Math.PI) / 180;
+function draw(canvas, ctx,){
+    //canvas.width = canvas.width;
+    var img = document.getElementById('shell');
+    ctx.fill();
+
+}
+function moveAndDraw(){
+    if( y > (canvas.height - 20)) direccion = -velocidad;
+    if(y < (20)) direccion = velocidad;
+    y += direccion;
+    draw(canvas,ctx,x,y);
 }
 
